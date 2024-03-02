370fea8b71530afe1e736ab3f6bd191a8b488d24
TienLam2003 <tienlamnguyen2003@gmail.com>
2024-03-02 11:58:19 +0700

    baitap

---
+++ CHUONG_4/Bai1_Chuong4/Bai1.js
@@ -0,0 +1,6 @@
+var ten, tuoi;
+ten = prompt("Bạn hãy nhập tên: ");
+tuoi = prompt("Bạn hãy nhập tuổi: ");
+document.write("Hello <B>" + ten + "</B>");
+document.write("<BR>");
+document.write("Tuổi <B>:" + tuoi + "</B>");
